a15be27991cec241604cf7d707e7a2b71b32076e
iann <iann.muller@kroton.tech>
2022-06-20 15:54:44 -0300

    add radius as porp

--- src/index.js
+++ src/index.js
@@ -75,6 +75,7 @@ function createAcceptButton(attributes = {}, events = {}) {
         backgroundColor: attributes.btnbgcolor,
         borderColor: attributes.btnbgcolor,
         color: attributes.btntextcolor,
+        borderRadius: attributes.btntBorderRadius,
       },
       events: {
         click: (e) => {
